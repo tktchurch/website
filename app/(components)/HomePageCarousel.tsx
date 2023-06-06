3dc92af5f72a39e5dac9f184df0fe28e78ad98af
zeel-codder <zeelprajapati321@gmail.com>
2023-06-06 22:13:58 +0530

    Update images of home page

--- app/(components)/HomePageCarousel.tsx
+++ app/(components)/HomePageCarousel.tsx
@@ -4,23 +4,25 @@ import { RxDotFilled } from "react-icons/rx";
 
 function HomePageCarousel() {
   const slides = [
-    {
-      url: "https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2620&q=80",
-    },
-    {
-      url: "https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80",
-    },
-    {
-      url: "https://images.unsplash.com/photo-1661961112951-f2bfd1f253ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2672&q=80",
-    },
-
-    {
-      url: "https://images.unsplash.com/photo-1512756290469-ec264b7fbf87?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2253&q=80",
-    },
-    {
-      url: "https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2671&q=80",
-    },
-  ];
+		{
+			url: "/assets/images/slider/1.jpg",
+		},
+		{
+			url: "/assets/images/slider/2.jpg",
+		},
+		{
+			url: "/assets/images/slider/3.jpeg",
+		},
+		{
+			url: "/assets/images/slider/4.jpeg",
+		},
+		{
+			url: "/assets/images/slider/5.jpeg",
+		},
+		{
+			url: "/assets/images/slider/6.jpg",
+		},
+	];
 
   const [currentIndex, setCurrentIndex] = useState(0);
 
