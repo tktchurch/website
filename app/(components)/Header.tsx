95a7ca1b6dd5aee32e86b6fc663081d008782b9f
Vamsi Madduluri <vamsi@dewonderstruck.com>
2023-07-13 18:56:10 +0530

    fix navigation header (#11)

--- app/(components)/Header.tsx
+++ app/(components)/Header.tsx
@@ -1,105 +1,130 @@
 "use client";
-import React, { useState } from 'react';
-import Link from "next/link";
+import { useState } from 'react';
+import Link from 'next/link';
 
 function Header() {
-    const [showMenu, setShowMenu] = useState(false)
-    const [showMenuForDesktop, setShowMenuForDesktop] = useState(true)
+    const [showMenu, setShowMenu] = useState(false);
 
-    let toggleShowMenu = () => {
-        setShowMenu(!showMenu)
-    }
+    const toggleShowMenu = () => {
+        setShowMenu(!showMenu);
+    };
 
-        return (
-            <header className=''>
-                <nav className="bg-[#000000] border-gray-200 px-4 lg:px-6 py-2.5">
-                    <div className="flex flex-wrap justify-evenly items-center mx-auto max-w-screen-xl">
-                        <Link href="/" className="flex items-center">
-                            <img src="/assets/images/logo.png" className="mr-3 h-24 sm:h-18"
-                                 alt="Logo"/>
-                            <span
-                                className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
-                        </Link>
-                        <div className="flex items-center lg:order-2">
-                            <button onClick={toggleShowMenu} data-collapse-toggle="mobile-menu-2" type="button"
-                                    className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-                                    aria-controls="mobile-menu-2" aria-expanded="false">
-                                <span className="sr-only">Open main menu</span>
-                                <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20"
-                                     xmlns="http://www.w3.org/2000/svg">
-                                    <path fillRule="evenodd"
-                                          d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                                          clipRule="evenodd"></path>
-                                </svg>
-                                <svg className="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20"
-                                     xmlns="http://www.w3.org/2000/svg">
-                                    <path fillRule="evenodd"
-                                          d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                                          clipRule="evenodd"></path>
-                                </svg>
-                            </button>
-                        </div>
-                        {showMenu &&
-                            <div className="justify-between items-center w-full lg:flex lg:w-auto lg:order-1 h-[80vh]"
-                                id="mobile-menu-2">
-                                <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
-                                    <li>
-                                        <Link onClick={toggleShowMenu} href="/"
-                                        className="block py-2 pr-4 pl-3 text-white rounded bg-primary-700 lg:bg-transparent lg:text-white lg:p-0 dark:text-white"
-                                        aria-current="page">Home
-                                        </Link>
-                                    </li>
-                                    <li>
-                                        <Link onClick={toggleShowMenu} href="/ministries"
-                                        className="block py-2 pr-4 pl-3 text-white border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-gray-300 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Ministries</Link>
-                                    </li>
-                                    <li>
-                                        <Link onClick={toggleShowMenu} href="/connect"
-                                            className="block py-2 pr-4 pl-3 text-white border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-gray-300 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Connect</Link>
-                                    </li>
-                                    <li>
-                                        <Link onClick={toggleShowMenu} href="/app"
-                                        className="block py-2 pr-4 pl-3 text-white border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-gray-300 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Our App</Link>
-                                    </li>
-                                    <li>
-                                        <Link onClick={toggleShowMenu} href="/giving"
-                                        className="block py-2 pr-4 pl-3 text-white border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-gray-300 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Give Online</Link>
-                                    </li>
-                                </ul>
-                            </div>
-                        }
-                        {showMenuForDesktop &&
-                            <div className=" hidden justify-between right-0 relative items-center w-full lg:flex lg:w-auto lg:order-1 "
-                                id="mobile-menu-2">
-                                <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0 lg:gap-12">
-                                    <li>
-                                        <Link href="/"
-                                        className="block py-2 pr-4 pl-3 text-white rounded bg-primary-700 lg:bg-transparent lg:text-white lg:p-0 dark:text-white"
-                                        aria-current="page">Home
-                                        </Link>
-                                    </li>
-                                    <li>
-                                        <Link href="/ministries"
-                                        className="block py-2 pr-4 pl-3 text-white border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-gray-300 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Ministries</Link>
-                                    </li>
-                                    <li>
-                                        <Link href="/connect"
-                                            className="block py-2 pr-4 pl-3 text-white border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-gray-300 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Connect</Link>
-                                    </li>
-                                    <li>
-                                        <Link href="/app"
-                                        className="block py-2 pr-4 pl-3 text-white border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-gray-300 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Our App</Link>
-                                    </li>
-                                    <li>
-                                        <Link href="/giving"
-                                        className="block py-2 pr-4 pl-3 text-white border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-gray-300 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Give Online</Link>
-                                    </li>
-                                </ul>
-                            </div>
-                        }
+    return (
+        <header>
+            <nav className="bg-black border-b border-gray-200 px-4 lg:px-6 py-2.5">
+                <div className="flex justify-between items-center mx-auto max-w-screen-xl">
+                    <Link href="/" passHref className="flex items-center">
+                            <img src="/assets/images/logo.png" className="mr-3 h-24 sm:h-18" alt="Logo" />
+                    </Link>
+                    <div className="flex items-center">
+                        <button
+                            onClick={toggleShowMenu}
+                            className="p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                            aria-controls="mobile-menu-2"
+                            aria-expanded={showMenu ? 'true' : 'false'}
+                        >
+                            <span className="sr-only">Open main menu</span>
+                            <svg
+                                className={`w-6 h-6 ${showMenu ? 'hidden' : ''}`}
+                                fill="currentColor"
+                                viewBox="0 0 20 20"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <path
+                                    fillRule="evenodd"
+                                    d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                                    clipRule="evenodd"
+                                />
+                            </svg>
+                            <svg
+                                className={`w-6 h-6 ${showMenu ? '' : 'hidden'}`}
+                                fill="currentColor"
+                                viewBox="0 0 20 20"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <path
+                                    fillRule="evenodd"
+                                    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                                    clipRule="evenodd"
+                                />
+                            </svg>
+                        </button>
                     </div>
-                </nav>
-            </header>
-        );
+                    <div
+                        className={`fixed top-0 left-0 h-screen w-screen bg-black z-50 transition-opacity duration-500 ${
+                            showMenu ? 'opacity-100 pointer-events-auto' : 'opacity-0 pointer-events-none'
+                        }`}
+                        onClick={toggleShowMenu}
+                    >
+                        <div className="flex flex-col justify-center h-full items-center">
+                            <ul className="flex flex-col text-center">
+                                <li>
+                                    <Link href="/" passHref onClick={toggleShowMenu}
+                                            className="block py-2 px-4 text-white rounded bg-primary-700 font-medium text-lg my-2 hover:bg-gray-900"
+                                        >
+                                            Home
+
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link href="/ministries" passHref onClick={toggleShowMenu}
+                                            className="block py-2 px-4 text-white rounded bg-primary-700 font-medium text-lg my-2 hover:bg-gray-900"
+                                        >
+                                            Ministries
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link href="/connect" passHref onClick={toggleShowMenu}
+                                            className="block py-2 px-4 text-white rounded bg-primary-700 font-medium text-lg my-2 hover:bg-gray-900"
+                                        >
+                                            Connect
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link href="/app" passHref onClick={toggleShowMenu}
+                                            className="block py-2 px-4 text-white rounded bg-primary-700 font-medium text-lg my-2 hover:bg-gray-900"
+                                        >
+                                            Our App
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link href="/giving" passHref onClick={toggleShowMenu}
+                                            className="block py-2 px-4 text-white rounded bg-primary-700 font-medium text-lg my-2 hover:bg-gray-900"
+                                        >
+                                            Give Online
+                                    </Link>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="hidden lg:block">
+                        <ul className="flex space-x-8">
+                            <li>
+                                <Link href="/" passHref className="text-white hover:text-white hover:underline hover:underline-offset-8">Home
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href="/ministries" passHref className="text-white hover:text-white hover:underline hover:underline-offset-8">Ministries
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href="/connect" passHref className="text-white hover:text-white hover:underline hover:underline-offset-8">Connect
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href="/app" passHref className="text-white hover:text-white hover:underline hover:underline-offset-8">Our App
+                                </Link>
+                            </li>
+                            <li>
+                                <Link href="/giving" passHref className="text-white hover:text-white hover:underline hover:underline-offset-8">Give Online
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </header>
+    );
 }
+
 export default Header;
